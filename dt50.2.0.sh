3f80f53d486e475f1cdf9e64e33c50e08d2c2802
qwertysamsungf700 <Patriot777>
2018-06-04 10:00:58 +0300

    Додав функцію копіювання з ПК на флешку у разі відсутньої директорії /Documents

--- dt50.2.0.sh
+++ dt50.2.0.sh
@@ -2,11 +2,21 @@
 # Створення константи на флеш память.
 FLASH="MyFiles"    #<---Назва флешки.
 user=$(whoami)     #<---Команда дізнається імя поточного користувача
-if [ -d /media/$user/$FLASH/Documents ]
+if [ -d /media/$user/$FLASH ]
 then
-echo "Копіювання файлів з флешки на ПК"
-cp -v /media/$user/$FLASH/Documents/*.* /home/$user/Документи/USB/
-echo "Копіювання завершено"
+echo "...flash connected..."
+	if [ -d /media/$user/$FLASH/Documents ]
+	then
+		echo "...copy file flash --> PC..."
+		cp $1 /media/$user/$FLASH/Documents/*.* /home/$user/Документи/USB/
+		echo "...copy complete..."
+	else
+		echo "...create folder Documents..."
+		mkdir /media/$user/$FLASH/Documents
+		echo "... copy file PC --> flash..."
+		cp -v /home/$user/Документи/USB/*.* /media/$user/$FLASH/Documents/
+		echo "...created and copy complete..."
+	fi		 
 else
-echo "Флешку не підключено"
+echo "...FLASH NOT FOUND..."
 fi
